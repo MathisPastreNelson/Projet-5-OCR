f967a534048e6677075d60336dfe54eec84de307
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-08-21 22:59:11 +0200

    Découverte de fetch+de son importation

--- front/js/script.js
+++ front/js/script.js
@@ -1,14 +1,36 @@
-// A lancer dans le terminal pour lancer le serveur back
-// cd back
-// node server
+//Fonction Récupération des datas du back
+let canapeData = [];
 
-const reponse = fetch("http://localhost:3000/api/products")
-    // quand tu as la réponse donne le résultat en json
-    .then(reponse => reponse.json())
-    // log du résultat dans la console
-    .then(reponse => console.table(reponse));
+async function fetchCanap() {
+    await fetch("http://localhost:3000/api/products")
+        // quand tu as la réponse donne le résultat en json
+        .then((response) => response.json())
+        // log du résultat dans la console
+        .then((promise) => {
+            canapeData = promise
+            console.table(canapeData);
+        })
+        // Si problème
+        .catch(error => console.log("Erreur de la base de donnée"))
+};
 
-//On vise l'ID dans le DOM
-document.getElementById("items")
 
+// Fonction pour mettre les cards dans la DIV html
+async function canapDisplay() {
+    await fetchCanap();
+    // Injection du HTML  
+    document.getElementById("items").innerHTML = canapeData.map((canapeData) =>
+        `
+        <a href = ${canapeData._id}>
+        <article>
+        <img src=${canapeData.imageUrl} alt=${canapeData.altTxt}>
+            <h3 class="productName">${canapeData.name}</h3>
+            <p class="productDescription">${canapeData.description}</p>
+    </article>
+    </a >
+    `
+    )
+}
 
+fetchCanap()
+canapDisplay()

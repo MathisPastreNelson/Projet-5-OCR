48cfecd43c6b991aab3496f60e8fd23ef3534ee1
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-08-27 17:24:07 +0200

    condition de l'ajout  au panier OK

--- front/js/product.js
+++ front/js/product.js
@@ -49,15 +49,40 @@ let productDisplay = async () => {
     });
     /*J'en suis là comprendre le localStorage*/
 
-    let getProductInBasket =
-        productPrice = canapeData.price
-    productTitle = canapeData.name
-    productColor = canapeData.colors
+    /* let getProductInBasket =
+         productPrice = canapeData.price
+     productTitle = canapeData.name
+     productColor = canapeData.colors
+ 
+     console.log(getProductInBasket)*/
 
-    console.log(getProductInBasket)
 };
 
-
+//Concernant le boutton "Ajouter au panier"
+const button = document.querySelector("button")
+//Condition vérifiant les valeurs que le client a choisi (couleur/quantité)
+if (button != null) {
+    button.addEventListener("click", (e) => {
+        const colorProductSelect = document.querySelector("#colors").value
+        const quantitySelect = document.querySelector("#quantity").value
+        //Condition contraignant l'utilisateur à mettre une couleur et une quantité de produit entre 1 et 100
+        if (colorProductSelect == null || colorProductSelect == "" ||
+            quantitySelect == 0 || quantitySelect > 100 || quantitySelect == null) {
+            alert("Selectionnez une couleur et un nombre d'article entre 1 et 100")
+        }
+        //Création d'un objet regroupant les informations du produit à transmettre en au panier
+        let productInBasket = {
+            id: idProduct,
+            name: canapeData.name,
+            imageUrl: canapeData.imageUrl,
+            imageTxt: canapeData.altTxt,
+            color: colorProductSelect,
+            quantity: quantitySelect,
+        }
+        //L'objet est ajouté au localStorage
+        localStorage.setItem(idProduct, JSON.stringify(productInBasket))
+    })
+}
 
 productDisplay();
 

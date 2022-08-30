3e51e62b7e31ae21054a990893e52453c0679b0e
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-08-31 01:52:04 +0200

    Changement de solution panier

--- front/js/product.js
+++ front/js/product.js
@@ -20,23 +20,23 @@ let productDisplay = async () => {
     //image
     document.querySelector(".item__img").innerHTML =
         ` 
-        <img src = ${canapeData.imageUrl} alt = ${canapeData.altTxt}>
-        `
+    <img src = ${canapeData.imageUrl} alt = ${canapeData.altTxt}>
+    `
     //prix
     document.getElementById("price").innerHTML =
         ` 
-        ${canapeData.price}
-        `
+    ${canapeData.price}
+    `
     //titre
     document.getElementById("title").innerHTML =
         `
-        ${canapeData.name}
-        `
+    ${canapeData.name}
+    `
     //description
     document.getElementById("description").innerHTML =
         `
-        ${canapeData.description}
-        `
+    ${canapeData.description}
+    `
     //couleur
     let select = document.getElementById('colors');
     //Fonction forEach pour afficher l'enssemble des colors
@@ -51,29 +51,29 @@ let productDisplay = async () => {
 
 //Concernant le boutton "Ajouter au panier"
 const button = document.querySelector("button")
-//Condition vérifiant les valeurs que le client a choisi (couleur/quantité)
-if (button != null) {
-    button.addEventListener("click", (e) => {
-        const colorProductSelect = document.querySelector("#colors").value
-        const quantitySelect = document.querySelector("#quantity").value
-        //Condition contraignant l'utilisateur à mettre une couleur et une quantité de produit entre 1 et 100
-        if (colorProductSelect == null || colorProductSelect == "" ||
-            quantitySelect <= 0 || quantitySelect > 100 || quantitySelect == null) {
-            alert("Selectionnez une couleur et un nombre d'article entre 1 et 100")
-        } else {
-            //Création d'un objet regroupant les informations du produit à transmettre en localstorage
-            let productInBasket = {
-                id: idProduct,
-                color: colorProductSelect,
-                quantity: Number(quantitySelect),
-            }
-            //L'objet est ajouté au localStorage avec son nom+couleur en guise de clé
-            localStorage.setItem(canapeData.name + " " + colorProductSelect, JSON.stringify(productInBasket))
-            alert("Votre produit a été ajouté au panier.")
-        }
-        //Récupérer le prix avec une autre fonction que le localStorage car danger de sécurité peut être un fetchproduct 
-    })
-}
+//Création d'event qui s'applique au clique de l'ajout au panier
+button.addEventListener("click", (e) => {
+    const colorProductSelect = document.querySelector("#colors").value
+    const quantitySelect = document.querySelector("#quantity").value
+    //Création d'un objet regroupant les informations du produit à transmettre en localstorage
+    let productInBasket = {
+        id: idProduct,
+        color: colorProductSelect,
+        quantity: Number(quantitySelect),
+    }
+    //Condition contraignant l'utilisateur à mettre une couleur + une quantité de produit entre 1 et 100
+    if (colorProductSelect == null || colorProductSelect == "" ||
+        quantitySelect <= 0 || quantitySelect > 100 || quantitySelect == null) {
+        alert("Selectionnez une couleur et un nombre d'article entre 1 et 100")
+    } else {
+        //L'objet est ajouté au localStorage avec son nom+couleur en guise de clé
+        localStorage.setItem(canapeData.name + " " + colorProductSelect, JSON.stringify(productInBasket))
+        alert("Votre produit a été ajouté au panier.")
+        console.log(productInBasket)
+    }
+    //Récupérer le prix avec une autre fonction que le localStorage car danger de sécurité peut être un fetchproduct 
+})
+
 
 productDisplay();
 

88b2690441ae1b433e7e53547285312009ae0a37
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-08-31 16:45:57 +0200

    Upload mentorat

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,11 +1,14 @@
 let getProduct = [];
 
-function RetrieveAllStorage() {
+
+//Fonction qui va chercher tout les produits du localStorage
+let RetrieveAllStorage = () => {
     let keys = Object.keys(localStorage);
     let i = keys.length;
+    //Tant qu'il y a des clés dans le localStorage je traduit et je met dans getProduct[]
     while (i--) {
-        let test = JSON.parse(localStorage.getItem(keys[i]))
-        getProduct.push(localStorage.getItem(keys[i]));
+        let parseAllProduct = JSON.parse(localStorage.getItem(keys[i]))
+        getProduct.push(parseAllProduct)
     }
 }
 

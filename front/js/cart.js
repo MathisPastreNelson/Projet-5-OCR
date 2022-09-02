9915f2449f3da33ec1348f0ed67825f748504717
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-09-02 18:44:36 +0200

    J'ai réussi à regrouper les datas des produits du panier
    Enfin !

--- front/js/cart.js
+++ front/js/cart.js
@@ -32,14 +32,10 @@ let getEveryProductInfo = async () => {
             ...getProductByFetch[i]
         });
     }
-    // console.table(getProduct)
-    // console.table(getProductByFetch)
-    console.table(everyProductInfo)
 }
 
 let displayProduct = async () => {
     await getEveryProductInfo()
-    console.log(getProduct)
     // Utilisation de la méthode "MAP" pour  chercher tout les objects et les placer dans l'ID
     document.getElementById("cart__items").innerHTML = everyProductInfo.map((everyProductInfo) =>
         `
@@ -64,9 +60,44 @@ let displayProduct = async () => {
     </div>
     </div>
     </article>
-    `
-    ).join("") //retire les ',' entre les cards
+    `).join("") //retire les ',' entre les cards
 };
 
+//JEN SUIS LA
+//Fonction pour additionner ou soustraire les products et leurs prix en modifiant la quantité
+let plusQuantity = async () => {
+    await displayProduct()
+    let quantityInput = document.querySelector('.itemQuantity');
+    console.log(quantityInput)
+    quantityInput.addEventListener("change", function (event) {
+        console.log(quantityInput.value)
 
-displayProduct()
+    }
+    )
+
+}
+
+
+plusQuantity()
+
+    // < !--  < article class="cart__item" data - id="{product-ID}" data - color="{product-color}" >
+    //             <div class="cart__item__img">
+    //               <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+    //             </div>
+    //             <div class="cart__item__content">
+    //               <div class="cart__item__content__description">
+    //                 <h2>Nom du produit</h2>
+    //                 <p>Vert</p>
+    //                 <p>42,00 €</p>
+    //               </div>
+    //               <div class="cart__item__content__settings">
+    //                 <div class="cart__item__content__settings__quantity">
+    //                   <p>Qté : </p>
+    //                   <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+    //                 </div>
+    //                 <div class="cart__item__content__settings__delete">
+    //                   <p class="deleteItem">Supprimer</p>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           </ > -->

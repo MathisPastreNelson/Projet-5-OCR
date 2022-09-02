b98a2bb02c8d30dab5d395b7544fb28635a96891
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-09-02 13:13:41 +0200

    Add files via upload

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,6 @@
 let getProduct = [];
 let getProductByFetch = [];
+let everyProductInfo = [];
 
 
 //Fonction qui va chercher tout les produits du localStorage
@@ -16,37 +17,46 @@ let retrieveAllStorage = async () => {
 //Fonction fectch dédiée au canapé de la page
 let productFetch = async () => {
     await retrieveAllStorage()
-    //il faut que j'arrive à contacter l'api de chaques produits du panier
-    Promise.all(getProduct.map(getProduct =>
+    //Je contacte les produit de L'API qui sont dans le localStorage pour obtenir l'enssemble des datas
+    getProductByFetch = await Promise.all(getProduct.map(getProduct =>
         fetch(`http://localhost:3000/api/products/${getProduct.id}`)
             .then(res => res.json())))
-
-        .then(console.log)
-    //J'en SUIS LA je n'arrive pas a récupérer la réponse dans getProductByFetch
-    console.log(getProductByFetch)
-
 };
 
-let displayProduct = async () => {
+//Fonction qui regroupe les données situées dans l'API relatif aux produits du panier
+let getEveryProductInfo = async () => {
     await productFetch()
+    for (let i = 0; i < getProduct.length; i++) {
+        everyProductInfo.push({
+            ...getProduct[i],
+            ...getProductByFetch[i]
+        });
+    }
+    // console.table(getProduct)
+    // console.table(getProductByFetch)
+    console.table(everyProductInfo)
+}
+
+let displayProduct = async () => {
+    await getEveryProductInfo()
     console.log(getProduct)
     // Utilisation de la méthode "MAP" pour  chercher tout les objects et les placer dans l'ID
-    document.getElementById("cart__items").innerHTML = getProduct.map((getProduct) =>
+    document.getElementById("cart__items").innerHTML = everyProductInfo.map((everyProductInfo) =>
         `
-    <article class="cart__item" data-id="${getProduct.id}" data-color="${getProduct.color}">
+    <article class="cart__item" data-id="${everyProductInfo.id}" data-color="${everyProductInfo.color}">
     <div class="cart__item__img">
-    <img src="." alt="Photographie d'un canapé">
+    <img src="${everyProductInfo.imageUrl}" alt="${everyProductInfo.altTxt}">
     </div>
     <div class="cart__item__content">
     <div class="cart__item__content__description">
-    <h2>Nom du produit</h2>
-    <p>${getProduct.color}</p>
-    <p>42,00 €</p>
+    <h2>${everyProductInfo.name}</h2>
+    <p>${everyProductInfo.color}</p>
+    <p>${everyProductInfo.price} €</p>
     </div>
     <div class="cart__item__content__settings">
     <div class="cart__item__content__settings__quantity">
-    <p>Qté : ${getProduct.quantity}</p>
-    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${getProduct.quantity}">
+    <p>Qté : ${everyProductInfo.quantity}</p>
+    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${everyProductInfo.quantity}">
     </div>
     <div class="cart__item__content__settings__delete">
     <p class="deleteItem">Supprimer</p>
@@ -58,5 +68,5 @@ let displayProduct = async () => {
     ).join("") //retire les ',' entre les cards
 };
 
+
 displayProduct()
-//console.table(getProduct)

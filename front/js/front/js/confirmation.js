e3dd4361ce0d2e8cdd41d1cf52bad6a5a78a397d
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-09-11 12:59:33 +0200

    Delete front/js/front directory

--- front/js/front/js/confirmation.js
+++
@@ -1,9 +0,0 @@
-// On récupère l'url de la page
-const url = new URL(window.location.href);
-// On récupère juste l'url ID
-const orderId = url.searchParams.get("orderId");
-// On met l'url ID dans le DOM
-document.getElementById("orderId").textContent = `${orderId}`;
-// On vide le localStorage pour que le panier revienne à zéro
-localStorage.clear();
-

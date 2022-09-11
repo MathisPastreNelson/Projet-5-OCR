b4b71ce7638e0ae8f5a0afc5779f4662678d78d2
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-09-11 12:51:17 +0200

    Fetch post OK + confirmation page OK ?
    ajustement de mon fetch post je récupère bien un orderid généré par le back que je met sur la page confirmation

---
+++ front/js/front/js/script.js
@@ -0,0 +1,33 @@
+// Variable qui récupère les données de l'API
+let canapeData = [];
+
+// Fonction de Récupération de l'API grâce a la méthode "FETCH"
+let fetchCanap = async () => {
+    await fetch("http://localhost:3000/api/products")
+        // La réponse convertie en .Json
+        .then((res) => res.json())
+        // Ajout de la fonction dans canapeData[]
+        .then((res) => canapeData = res)
+    console.table(canapeData)
+};
+
+// Fonction d'affichage des canapés
+let canapDisplay = async () => {
+    await fetchCanap();
+    // Utilisation de la méthode "MAP" pour chercher tout les objects et les placer dans l'ID
+    document.getElementById("items").innerHTML = canapeData.map((canapeData) =>
+        `
+    <a href = ./product.html?id=${canapeData._id}>
+    <article>
+    <img src=${canapeData.imageUrl} alt=${canapeData.altTxt}>
+    <h3 class="productName">${canapeData.name}</h3>
+    <p class="productDescription">${canapeData.description}</p>
+    </article>
+    </a >
+    `
+    ).join("") // Retire les ',' entre les cards
+};
+
+canapDisplay();
+
+

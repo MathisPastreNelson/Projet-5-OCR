5e548f885de3941af7d229371b38e114de883aa9
MathisP <103594397+MathisPastreNelson@users.noreply.github.com>
2022-09-15 19:09:29 +0200

    Projet ok (v1.0)

---
+++ front/front/js/script.js
@@ -0,0 +1,33 @@
+// Variable qui récupère les données de l'API
+let canapeData = [];
+
+// Fonction de Récupération de l'API grâce a la méthode "FETCH"
+let fetchCanap = async () => {
+    await fetch("http://localhost:3000/api/products/")
+        // La réponse converti en .Json
+        .then((res) => res.json())
+        // Ajout de la fonction dans canapeData[]
+        .then((res) => canapeData = res)
+    console.table(canapeData)
+};
+
+// Fonction d'affichage des canapés
+let canapDisplay = async () => {
+    await fetchCanap();
+    // Utilisation de la méthode "MAP" pour chercher tous les objects et les placer dans l'ID
+    document.querySelector("#items").innerHTML = canapeData.map((canapeData) =>
+        `
+    <a href = ./product.html?id=${canapeData._id}>
+    <article>
+    <img src=${canapeData.imageUrl} alt=${canapeData.altTxt}>
+    <h3 class="productName">${canapeData.name}</h3>
+    <p class="productDescription">${canapeData.description}</p>
+    </article>
+    </a >
+    `
+    ).join("") // Retire les ',' entre les cards
+};
+
+canapDisplay();
+
+
